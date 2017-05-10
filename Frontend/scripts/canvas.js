41c4cfccf8f4c65b96497d461f2813bc44d5f788
Luis Fer <G.G.LuisFer@gmail.com>
2017-05-09 20:33:17 -0500

    Interfaz para dibujar

--- Frontend/scripts/canvas.js
+++ Frontend/scripts/canvas.js
@@ -1,1 +1,43 @@
-var sketcher = new atrament(document.getElementById('canvasPaint'));
+var height = $(document.getElementById('divCanvas')).height();
+var width = $(document.getElementById('divCanvas')).width();
+var sketcher = atrament(document.getElementById('canvasPaint'),width,500);
+
+document.getElementById("crearFigura").onclick = function() {crearFigura()};
+document.getElementById("limpiarCanvas").onclick=function(){limpiar()};
+
+function crearFigura() {
+	var c=document.getElementById("canvasPaint");
+	var ctx=c.getContext("2d");
+	var l1 = $('#lado1').val();
+	var l2 = $('#lado2').val();
+	var l3 = $('#lado3').val();
+	if (l1!=0&&l2!=0&&l3!=0) {
+		ctx.beginPath();
+		ctx.moveTo(20, 20);
+		ctx.lineTo(20, l1);
+		ctx.lineTo(l1, l2);
+		ctx.closePath();
+		 
+		// the outline
+		ctx.lineWidth = 4;
+		ctx.strokeStyle = '#666666';
+		ctx.stroke();
+	} 
+	else if (l1!=0&&l2!=0&&l3==0) {
+		ctx.rect(20,20,l2,l1);
+		ctx.stroke();
+	}
+	else if (l1!=0&&l2==0&&l3==0) {
+		ctx.rect(20,20,l1,l1);
+		ctx.stroke();
+	}
+}
+
+function limpiar(){
+	var c=document.getElementById("canvasPaint");
+	var ctx=c.getContext("2d");
+	ctx.clearRect(0, 0, width, 500);
+	location.reload();
+	//sketcher.clear();
+}
+
