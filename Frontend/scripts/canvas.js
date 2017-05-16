5b565263e2636179086f8581065740791db5ff40
Luis Fer <G.G.LuisFer@gmail.com>
2017-05-16 01:36:00 -0500

    Pequeño cambio frontend

--- Frontend/scripts/canvas.js
+++ Frontend/scripts/canvas.js
@@ -11,7 +11,6 @@ function crearFigura() {
 	var l1 = $('#lado1').val();
 	var l2 = $('#lado2').val();
 	var l3 = $('#lado3').val();
-	ctx.clearRect(0, 0, 500, 500);
 	if (l1!=0&&l2!=0&&l3!=0) {
 		ctx.beginPath();
 		ctx.moveTo(20, 20);
@@ -26,10 +25,14 @@ function crearFigura() {
 	} 
 	else if (l1!=0&&l2!=0&&l3==0) {
 		ctx.rect(20,20,l2,l1);
+		ctx.lineWidth = 4;
+		ctx.strokeStyle = '#666666';
 		ctx.stroke();
 	}
 	else if (l1!=0&&l2==0&&l3==0) {
 		ctx.rect(20,20,l1,l1);
+		ctx.lineWidth = 4;
+		ctx.strokeStyle = '#666666';
 		ctx.stroke();
 	}
 }
